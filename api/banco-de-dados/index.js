107008efa877ebc624a5c6e2c24f610ff16323ef
danielemaraschin <danikmaraschin@gmail.com>
2021-04-14 13:49:49 +0200

    commit fully

---
+++ api/banco-de-dados/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const config = require('config')
+
+const instancia = new Sequelize(
+    'petshop',
+    'root',
+    'admin',
+    {
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    }
+)
+
+module.exports = instancia
