1f812813eeb19fdb21b0c671b369cd0ac70620d5
danielemaraschin <danikmaraschin@gmail.com>
2021-04-17 10:56:49 +0200

    create table, module.export, db.define

---
+++ api/rotas/fornecedores/produtos/ModeloTabelaProduto.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+const instancia = require('../../../banco-de-dados') // instancia do db
+const colunas = {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    preco: {
+        type: Sequelize.DOUBLE, //2 casas dps da virgula
+        allowNull: false
+    },
+    estoque: {
+        type: Sequelize.INTEGER, //numerointeiro
+        allowNull: false,
+        defaultValue: 0
+    }
+}
+
+const opcoes = {
+    freezeTableName: true,
+    tableName: 'produtos',
+    timestamps: true, //ativando marcas de tempo
+    createdAt: 'dataCriacao',
+    updatedAt: 'dataAtualizacao',
+    version: 'versao'
+}
+//so exportar 'colunas' nao vai criar a tb pq precisa conectar com a instancia do db 
+//entao exportar ligando com o banco de dados
+//define a instancia do sequelize é definir o modelo da tabela
+module.exports = instancia.define('produto', colunas, opcoes)
+//1param: nome do modelo /2param: colunas/3param: outras config
