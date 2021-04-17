cffa8adf1fb056b5acf7a097308311d079a661ac
danielemaraschin <danikmaraschin@gmail.com>
2021-04-17 18:28:52 +0200

    DAO

---
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -0,0 +1,12 @@
+const Modelo = require('./ModeloTabelaProduto') //esse Modelo do sequelize para se comunicar com o db
+
+
+module.exports = {
+    listar(idFornecedor) { //retorna nossa lista de produtos - mas nao queremos a lista inteira
+        return Modelo.findAll({ // queremos somente os produtos de um determinado fornecedor
+            where: {        //passa um objeto 'where'e dentro as intruções para encontrar o produto
+                fornecedor: idFornecedor//campo fornecedor do produto deve ser igual ao idFornecedor do fornecedor
+            }
+        })
+    }
+}
