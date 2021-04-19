44cc44b39d07c80d29e63b3e5c6f0263b43aadbc
danielemaraschin <danikmaraschin@gmail.com>
2021-04-19 19:13:05 +0200

    organizing the code

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -8,8 +8,36 @@ roteador.get('/', async(requisicao, resposta) => {
     )
 })
 
+roteador.post('/', (requisicao, resposta) => {
+    const idFornecedor = require.params.idFornecedor
+    resposta.send(JSON.stringify())
+})
+
+roteador.post('/', async (requisicao, resposta, proximo) => {
+    try {
+        const dadosRecebidos = requisicao.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        resposta.status(201)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type')
+        )
+        resposta.send(
+            serializador.serializar(fornecedor)
+        )
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+
+
+
+
 const roteadorReclamacoes = require('./reclamacoes')
 
 roteador.use('/:idProduto/reclamacoes', roteadorReclamacoes)
 
 module.exports = roteador
+
+/*arquivo de rota de produtos*/
