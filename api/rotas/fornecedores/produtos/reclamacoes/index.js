8da8d36d22247a5642db77edabaaaf9531dac5e8
danielemaraschin <danikmaraschin@gmail.com>
2021-04-19 09:06:16 +0200

    creating a new route

---
+++ api/rotas/fornecedores/produtos/reclamacoes/index.js
@@ -0,0 +1,9 @@
+const roteador = require('express').Router()
+const Tabela = require('./TabelaProduto')
+
+roteador.get('/', (requisicao, resposta) => {
+    resposta.send(
+        JSON.stringify([])
+    )
+})
+module.exports = roteador
