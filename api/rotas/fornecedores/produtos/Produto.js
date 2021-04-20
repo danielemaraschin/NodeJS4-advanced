6efbc869f933a72da419a0104fff060bc0200ae6
danielemaraschin <danikmaraschin@gmail.com>
2021-04-20 11:11:57 +0200

    working with : constructor new class

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -1,4 +1,16 @@
-class Produto {}
+class Produto {
+    //constructor pega dados que recebemos e passa para uma instancia 
+    constructor({id, titulo, preco, estoque, fornecedor, dataCriacao, dataAtualizacao, versao}) {
+        this.id = id
+        this.titulo = titulo
+        this.preco = preco
+        this.estoque = estoque
+        this.fornecedor = fornecedor
+        this.dataCriacao = dataCriacao
+        this.dataAtualizacao = dataAtualizacao
+        this.versao = versao
+    }
+}
 
 
 module.exports = Produto
