00ff19da699462f215bdca1f6cb5a8a41b4c71ac
danielemaraschin <danikmaraschin@gmail.com>
2021-05-07 20:28:56 +0200

    exercise

--- exercicios/quiz/questions.js
+++ exercicios/quiz/questions.js
@@ -7,3 +7,16 @@ const colunas = {
 }
 
 module.exports = instancia.define('pergunta', colunas)
+/*
+Para representar a coleção de perguntas, deve definir uma rota /api/perguntas,
+ e para acessar cada documento da coleção, /api/perguntas/:idPergunta. 
+ A coleção de respostas, fica em /api/perguntas/:idPergunta/respostas, 
+e para cada documento, /api/perguntas/:idPergunta/respostas/:idResposta.
+
+
+
+Alternativa correta! Com isso, conseguimos usar corretamente as coleções de documentos, 
+seguidas do identificador de cada documento,
+ finalizando com os stores (sub coleções) e seus documentos.
+
+*/
